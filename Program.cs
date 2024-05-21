d3a293b15668a29a6cef395e127331812ae41db1
Ashurov Safarmurod <sm.ashurov7@gmail.com>
2024-05-21 17:41:16 +0500

    Closes #2

--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
+using FilesApi.DataAccessLayer;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +11,18 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Register MongoDB settings
+var mongoDbSettings = builder.Configuration.GetSection("MongoDbSettings");
+builder.Services.Configure<MongoDbSettings>(mongoDbSettings);
+
+// Register MongoDB client
+builder.Services.AddSingleton<IMongoClient, MongoClient>(sp =>
+{
+    var settings = sp.GetRequiredService<IOptions<MongoDbSettings>>().Value;
+    return new MongoClient(settings.ConnectionString);
+});
+
+builder.Services.AddScoped<IRepository, Repository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
